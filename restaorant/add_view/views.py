9d843b25cd4f03652ff2ad2bc4ca5fb4e100f2db
mazzanek <158159078+mazzanek@users.noreply.github.com>
2024-07-16 13:58:53 +0200

    frame and model works

--- restaorant/add_view/views.py
+++ restaorant/add_view/views.py
@@ -2,6 +2,12 @@ from django.http import HttpResponse
 from django.template import loader
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
+from .models import add_view
+
 def home(request):
+  all_rest = add_view.objects.all().values()
   template = loader.get_template('home.html')
-  return HttpResponse(template.render())
+  context = {
+    'all_rest': all_rest ,
+  }
+  return HttpResponse(template.render(context, request))
